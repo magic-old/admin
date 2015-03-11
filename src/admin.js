3c3979f291e0b288cd46f8b3e05b927cdfb35a4f
Jascha Ehrenreich <jascha@jaeh.at>
2015-03-11 21:07:59 +0100

    minor changes

--- src/admin.js
+++ src/admin.js
@@ -1,40 +1,35 @@
 'use strict';
-var express = require('express')
-  , path    = require('path')
-  , css     = require('stylus')
-  , admin   = {}
-;
+import express from 'express';
+import {join} from 'path';
+import stylus from 'stylus';
 
-function static(req, res) {
-  var app = req.app
-    , dir = __dirname + '/public/'
-  ;
-
-  app.use( css.middleware({
-      src: dir
-    , dest: path.join(app.get('dirs').public, 'css')
-    , maxage: '1d'
-    , compile: function compile(str, path) {
-      return css(str)
-              .set('filename', path)
-              .set('compress', app.get('env') === 'production' )
-              .use(nib())
-              .import('nib')
+function staticFiles(req, res) {
+  var css = stylus
+    , options = {
+        src: join(__dirname, 'public')
+      , dest: join( req.app.get('dirs').public, 'css' )
+      , maxage: '1d'
+      , compile: (str, path) => {
+          return css(str)
+            .set('filename', path)
+            .set('compress', req.app.get('env') === 'production' )
+            .use(nib())
+            .import('nib')
         ;
       }
-  }) );
+    }
+  ;
+
+  req.app.use( css.middleware(options) );
 }
 
-export function init = function adminInit(req, res, next) {
-  req.app.set('admin', true);
-
-  res.render(path.join(__dirname, 'views', 'menu.jade'), (err, html) => {
-    //~ console.log('html', html);
-    if ( html ) {
-      res.locals.adminMenu = html;
-      res.locals.adminCssPath = '/css/admin.css';
-      static(req, res);
-    }
+export function init(req, res, next) {
+  res.render( join(__dirname, 'views', 'menu.jade'), (err, html) => {
+    if ( ! html ) { return next(); }
+    
+    res.locals.adminMenu = html;
+    res.locals.adminCssPath = '/css/admin.css';
+    staticFiles(req, res);
     next();
   });
 }

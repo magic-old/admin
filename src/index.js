f85edbcac8cd5499e0cbd9b03a6a8d0cb7a37440
Jascha Ehrenreich <jascha@jaeh.at>
2016-03-14 01:05:46 +0100

    es7ify

--- src/index.js
+++ src/index.js
@@ -1,35 +1,40 @@
-'use strict';
-import express from 'express';
-import {join} from 'path';
+import { join } from 'path';
 import stylus from 'stylus';
+import nib from 'nib';
 
-function staticFiles(req, res) {
-  var css = stylus
-    , options = {
-        src: join(__dirname, 'public')
-      , dest: join( req.app.get('dirs').public, 'css' )
-      , maxage: '1d'
-      , compile: (str, path) => {
-          return css(str)
-            .set('filename', path)
-            .set('compress', req.app.get('env') === 'production' )
-            .use(nib())
-            .import('nib')
-        ;
+const noop = () => {};
+
+export const staticFiles =
+  (req, res) => {
+    const css = stylus;
+
+    const options = {
+      src: join(__dirname, 'public'),
+      dest: join(req.app.get('dirs').public, 'css'),
+      maxage: '1d',
+      compile: (str, path) => {
+        return css(str)
+          .set('filename', path)
+          .set('compress', req.app.get('env') === 'production')
+          .use(nib())
+          .import('nib');
+      },
+    };
+
+    req.app.use(css.middleware(options));
+  };
+
+export const init =
+  (req, res, next = noop) => {
+    const template = join(__dirname, 'views', 'menu.jade');
+    res.render(template, (err, html) => {
+      if (err || !html) {
+        return next(err);
       }
-    }
-  ;
 
-  req.app.use( css.middleware(options) );
-}
-
-export function init(req, res, next) {
-  res.render( join(__dirname, 'views', 'menu.jade'), (err, html) => {
-    if ( ! html ) { return next(); }
-    
-    res.locals.adminMenu = html;
-    res.locals.adminCssPath = '/css/admin.css';
-    staticFiles(req, res);
-    next();
-  });
-}
+      res.locals.adminMenu = html;
+      res.locals.adminCssPath = '/css/admin.css';
+      staticFiles(req, res);
+      next();
+    });
+  };
